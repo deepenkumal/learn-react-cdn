d20ab1058357f7ae560116f1599a13046e67adfc
deepenkumal <deepenkumal@noreply.github.com>
2021-05-13 15:44:13 +0545

    react create element added

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const rootElement = document.getElementById("root");
 //React is used to create element 
 const myFirstElement = React.createElement('ul',null,
 [
-React.createElement('li',null,Apple),
-React.createElement('li',null,Banana),
+React.createElement('li',null,'Apple'),
+React.createElement('li',null,'Banana'),
 
 ]);
 //ReactDOM is used to render reat element
